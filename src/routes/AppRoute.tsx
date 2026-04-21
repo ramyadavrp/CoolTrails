f2616f22918a66a6857020738aa7a88a900d2f53
unknown <mr.ramprakash93@gmail.com>
2026-04-21 14:45:09 +0530

    issue explore community btn home

--- src/routes/AppRoute.tsx
+++ src/routes/AppRoute.tsx
@@ -92,7 +92,7 @@ const AppRoute = () => {
         <Route path="/complete" element={<Complete />} />
         <Route path="/trail-map" element={<FullTrailMapSection />} />
         {/* 30-1-26 add  */}
-        <Route path="/community" element={<CommunityPage />} /> 
+        {/* <Route path="/community" element={<CommunityPage />} />  */}
           
         
 {/* <Route path="/guides/:country/:title" element={<ParkDetails />} />  15-12-25*/} 
@@ -121,7 +121,7 @@ const AppRoute = () => {
           <Route path="/profile-feed" element={<ProfilefeedPage />} />
           {/* <Route path="/complete-trail" element={<CompleteTrailPage />} /> */}
           <Route path="/profile-photo" element={<ProfilePhotos />} />
-          {/* <Route path="/community" element={<CommunityPage />} /> */}
+          <Route path="/community" element={<CommunityPage />} />
           <Route path="/explore/recording/:slug" element={<CommunityPageCmtDetails />} />
           <Route path="/add-post" element={<AddPost />} />
           
